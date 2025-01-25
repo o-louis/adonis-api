00f3b3c6f434f5ac6e8009e6ca12bb699dbe8f01
o-louis <oriane.louis.pro@gmail.com>
2025-01-24 19:17:46 -0500

    build: Setup adonisjs

---
+++ start/routes.ts
@@ -0,0 +1,36 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+const UsersController = () => import('#controllers/users_controller')
+
+const AuthController = () => import('#controllers/auth_controller')
+
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
+
+// SIGN IN
+router.post('/register', [AuthController, 'register'])
+router.post('/login', [AuthController, 'login'])
+router.delete('/logout', [AuthController, 'logout']).use(middleware.auth())
+router.get('/me', [AuthController, 'me'])
+
+// USERS
+router
+  .group(() => {
+    router.get('/users', [UsersController, 'index'])
+    router.get('/users/:id', [UsersController, 'show'])
+    router.put('/users/:id', [UsersController, 'update']).use(middleware.ensureOwnership())
+    router.delete('/users/:id', [UsersController, 'destroy']).use(middleware.ensureOwnership())
+  })
+  .use(middleware.auth())
