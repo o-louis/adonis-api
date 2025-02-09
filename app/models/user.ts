48eb1834ccc8acc977be2b4ef9b0078f4c8acc87
o-louis <oriane.louis.pro@gmail.com>
2025-02-09 16:19:16 -0500

    feat: Create user role

--- app/models/user.ts
+++ app/models/user.ts
@@ -5,6 +5,11 @@ import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
   passwordColumnName: 'password',
@@ -22,6 +27,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column({ serializeAs: null })
   declare password: string
+
+  @column()
+  declare role: UserRole
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
--- database/migrations/1737756700719_create_users_table.ts
+++ database/migrations/1737756700719_create_users_table.ts
@@ -1,3 +1,4 @@
+import { UserRole } from '#models/user'
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
@@ -9,6 +10,7 @@ export default class extends BaseSchema {
       table.string('name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
+      table.enum('role', Object.values(UserRole)).notNullable().defaultTo(UserRole.USER)
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
